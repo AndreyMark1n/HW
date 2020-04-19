c354ed4e06cb47d8c3e18f3a099d29e9f935d9e6
AndreyMark1n <63951824+AndreyMark1n@users.noreply.github.com>
2020-04-19 13:00:53 +0300

    Add files via upload

---
+++ razvorot.js
@@ -0,0 +1,13 @@
+Array.prototype.razvorot = function(){
+    let arr = [];
+    for( let i = this.length; i--; ){
+        arr.push( this[i] );
+    };
+    return arr;
+};
+
+let a = [1, 2, 3, 4, 5, 6, 7, 8,];
+let b = a.razvorot();
+
+alert(a);
+alert(b);
